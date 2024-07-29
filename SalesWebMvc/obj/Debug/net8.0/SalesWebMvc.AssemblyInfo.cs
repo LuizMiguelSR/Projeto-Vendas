f3f082feeaef20c649741443eae460908a16a1fe
Luiz Miguel Santos Rodrigues <luiz.miguel@kbrtec.com.br>
2024-07-29 18:49:49 -0300

    Up: SellerFormViewModel and Department select component

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60eb364c4c455001427a083076dbfff057ce376b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f52c8c774c301d3a99c5222f18f2b9ff1cd8d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
